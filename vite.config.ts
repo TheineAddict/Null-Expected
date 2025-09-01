1dbe95716fa7b6553710787097ac8da8d56b1df8
TheineAddict <43204663+TheineAddict@users.noreply.github.com>
2025-09-01 11:03:14 +0300

    Fix lucide-react import error

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
   test: {
     globals: true,
     environment: 'jsdom',
