93c3ec90bd06d0a32387ef9ad3bc8fb1de64c578
TheineAddict <43204663+TheineAddict@users.noreply.github.com>
2025-09-01 10:10:35 +0300

    Add linkedin author image reference

--- src/config/authors.ts
+++ src/config/authors.ts
@@ -19,7 +19,7 @@ export const AUTHORS: Record<string, Author> = {
     title: 'Release Manager',
     bio: 'With 12 years of experience in QA and release management, Ade has worked across financial software platforms, leading large-scale migrations and building efficient, streamlined release strategies. Formerly a QA Manager, she brings a strong focus on embedding quality early, fostering team alignment, and balancing agility with risk-based decision making. Ade is passionate about quality culture, pragmatic processes, and helping QA professionals grow their careers. Outside of work, she blends her love of tech, writing, and creativity through projects like Null Expected.',
     tag: '[ quality_mindset = true ]',
-    imageUrl: 'https://images.pexels.com/photos/3184291/pexels-photo-3184291.jpeg?auto=compress&cs=tinysrgb&w=400',
+    imageUrl: 'https://media.licdn.com/dms/image/v2/D4D03AQEOv7QBIbhwjA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1723039271624?e=1759363200&v=beta&t=fNqH3lggh-LVs-TpVJSbsYEX_lwOaFvDcQwcqTJT9gI',
     linkedinUrl: 'https://www.linkedin.com/in/adevitan',
     email: 'ade@nullexpected.com'
   },
