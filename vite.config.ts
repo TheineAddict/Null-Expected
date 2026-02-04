2fc743cf37937085080713279a3cd093dc673b84
TheineAddict <43204663+TheineAddict@users.noreply.github.com>
2026-02-04 15:08:38 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-/// <reference types="vitest" />
-/// <reference types="vitest" />
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +7,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
     include: ['src/tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 });
