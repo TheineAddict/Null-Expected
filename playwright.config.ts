ed0ceffda8d4854dc5b8c4e93c4d788a1c9c15b5
TheineAddict <43204663+TheineAddict@users.noreply.github.com>
2026-02-09 14:34:19 +0200

    Fixed the playwright paths

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests/e2e',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 1,
@@ -18,7 +17,18 @@ export default defineConfig({
 
   projects: [
     {
-      name: 'chromium',
+      name: 'e2e',
+      testDir: './tests/e2e',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'visual',
+      testDir: './tests/visual',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'api',
+      testDir: './tests/api',
       use: { ...devices['Desktop Chrome'] },
     },
   ],
